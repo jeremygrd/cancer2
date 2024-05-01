c40e3d5a985251d0209d1e2c825c516ddb8d67b5
jeremygrd <33085744+jeremygrd@users.noreply.github.com>
2024-05-01 14:58:28 +0000

    final

--- cancer_prediction/cancer_model.py
+++ cancer_prediction/cancer_model.py
@@ -43,7 +43,7 @@ class CancerModel:
             "pca__n_components": np.arange(1, 31),
             "model__C": np.logspace(-3, 1, 100),
         }
-        
+
         grid = GridSearchCV(pipe, param_grid, cv=5, n_jobs=-1, scoring="accuracy", verbose=1)
         grid.fit(X, y)
 
@@ -147,6 +147,7 @@ class CancerModel:
     def get_feature_variance(self):
         return self.pca.explained_variance_ratio_
 
+    
     def get_feature_importance_df(self, X):
         feature_importance = self.get_feature_importance()
         feature_variance = self.get_feature_variance()
